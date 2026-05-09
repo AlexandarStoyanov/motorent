f99d78aabf2020399a5d379d25933a23565865cc
Александър Стоянов <stu2201321048@uni-plovdiv.bg>
2026-05-10 02:08:38 +0300

    Fix email imports

--- tours/api.py
+++ tours/api.py
@@ -2,8 +2,7 @@ import json
 import stripe
 
 from django.conf import settings
-from django.core.mail import send_mail
-from django.http import JsonResponse
+from django.core.mail import get_connection, EmailMessagefrom django.http import JsonResponse
 from django.views.decorators.csrf import csrf_protect
 from django.views.decorators.http import require_POST
 from rest_framework.viewsets import ReadOnlyModelViewSet
